9cf555d9674cce524ce7755022874e412452ae00
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2022-07-25 18:56:45 +0400

    part2 form phonebook solution.

---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,13 @@
+function Persons({ persons }) {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Persons;
